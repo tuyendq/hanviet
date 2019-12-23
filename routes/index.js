f80410a0cf9f64aac62299f0065789f136fb3a04
Tuyen Dang <tuyendq@gmail.com>
2019-12-23 11:24:13 +0700

    add fields to json file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const HanziWriter = require('hanzi-writer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,14 +10,6 @@ router.get('/', function(req, res, next) {
 // ];
   var characters = require('../data/characters.json');
   var charOfDay = characters[Math.floor(Math.random() * characters.length)];
-
-  // var writer = HanziWriter.create('char-target-div', '我', {
-  //   width: 600,
-  //   height: 600,
-  //   padding: 16,
-  //   strokeColor: '#000000',
-  //   radicalColor: '#168F16'
-  // });
 
   res.render('pages/index', {
     title: 'Học từ Hán Việt',
