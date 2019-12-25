0a792c2003b333cab0511d39aa2bd3b9e1e2bac4
Tuyen Dang <tuyendq@gmail.com>
2019-12-25 21:12:00 +0700

    routes/index.js: add router /:id

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var characters = require('../data/characters.json');
+const charDefault = {id: 0, cn: "和", vi: "hòa"}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,14 +11,29 @@ router.get('/', function(req, res, next) {
 //   { id: 2, ch: '一' },
 //   { id: 3, ch: '了' }
 // ];
-  var characters = require('../data/characters.json');
+  // var characters = require('../data/characters.json');
   var charOfDay = characters[Math.floor(Math.random() * characters.length)];
 
   res.render('pages/index', {
     title: 'Học từ Hán Việt',
-    characters: characters,
+    // characters: characters,
     charOfDay: charOfDay
   });
 });
+
+/* GET character by id */
+router.get('/:id', function(req, res, next) {
+  const charId = parseInt(req.params.id)
+  let charOfDay = characters.find((char) => {
+    return char.id == charId
+  })
+  if (!charOfDay) {charOfDay = charDefault}
+
+  res.render('pages/index', {
+    title: 'Học từ Hán Việt',
+    charOfDay: charOfDay
+  })
+  
+})
 
 module.exports = router;
