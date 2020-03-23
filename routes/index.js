1896aa64214ae4fa38de37b2a252883c00f4382e
Tuyen Dang <tuyendq@gmail.com>
2020-03-23 21:30:18 +0700

    run npm audit fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var characters = require('../data/characters.json');
 const charDefault = {id: 0, cn: "和", vi: "hòa"}
+const charDefault2 = {id: 0, cn: "花", vi: "hoa"}
 
 
 /* GET home page. */
@@ -27,13 +28,27 @@ router.get('/:id', function(req, res, next) {
   let charOfDay = characters.find((char) => {
     return char.id == charId
   })
-  if (!charOfDay) {charOfDay = charDefault}
+  if (!charOfDay) {charOfDay = charDefault2}
 
   res.render('pages/index', {
     title: 'Học từ Hán Việt',
     charOfDay: charOfDay
   })
   
+})
+
+/* GET /search?q= */
+router.get('/search', (req, res, next) => {
+  let charOfDay = characters.find((char) => {
+    return char.cn == req.query.term
+  })
+  // if (!charOfDay) {charOfDay = charDefault}
+
+  res.render('pages/index', {
+    title: 'Test', //req.query.term, // 'Học từ Hán Việt',
+    charOfDay: charOfDay
+  })
+
 })
 
 module.exports = router;
